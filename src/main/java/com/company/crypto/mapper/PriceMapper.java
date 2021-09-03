24f4c0f283dac33ccc435c450f10d7a36ae7ae84
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-09-03 22:43:20 +0300

    deleted imports

--- src/main/java/com/company/crypto/mapper/PriceMapper.java
+++ src/main/java/com/company/crypto/mapper/PriceMapper.java
@@ -1,10 +1,7 @@
 package com.company.crypto.mapper;
 
 import com.company.crypto.dto.PriceDto;
-import com.company.crypto.dto.UserDto;
 import com.company.crypto.entity.Price;
-import com.company.crypto.entity.User;
-import com.sun.scenario.effect.impl.prism.PrDrawable;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
