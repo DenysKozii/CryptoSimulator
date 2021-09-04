dae12f0448d1c0f2e0d1e704dd2792351573e5ec
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-09-04 15:42:48 +0300

    added profile page

---
+++ src/main/java/com/company/crypto/dto/AssetDto.java
@@ -0,0 +1,19 @@
+package com.company.crypto.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+@Data
+@Builder
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class AssetDto extends BaseDto{
+    private String symbol;
+    private Double amount;
+    private Double sum;
+}
