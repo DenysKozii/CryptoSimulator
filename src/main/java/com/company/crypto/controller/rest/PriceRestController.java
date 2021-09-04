36602d107e5c598d056e7a04021436c7a523c221
DenysKozii <denys.kozii@gmail.com>
2021-09-05 00:25:53 +0300

    add PriceRestController

---
+++ src/main/java/com/company/crypto/controller/rest/PriceRestController.java
@@ -0,0 +1,27 @@
+package com.company.crypto.controller.rest;
+
+import com.company.crypto.dto.PriceDto;
+import com.company.crypto.dto.UserDto;
+import com.company.crypto.service.PriceService;
+import com.company.crypto.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/v1")
+public class PriceRestController {
+    private final PriceService priceService;
+
+    @GetMapping("/list")
+    public String getAllPrices(Model model) {
+        List<PriceDto> prices = priceService.getAll();
+        model.addAttribute("prices", prices);
+        return "pricesList";
+    }
+}
